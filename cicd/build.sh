832f4a41744a71c93d399d9d7d2a1dc145bee623
jsecchiero <secchierojacopo@gmail.com>
2019-04-08 23:36:46 +0200

    upgrade to ansible 2.7.10

--- cicd/build.sh
+++ cicd/build.sh
@@ -5,7 +5,7 @@ set -x
 
 # Prerequisuite
 
-pip install ansible==2.6.2
+pip install ansible==2.7.10
 wget -O packer.zip https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_amd64.zip
 unzip -o packer.zip
 sudo apt-get update
