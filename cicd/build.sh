b53746a83bd38390ccd92108868fb01b6c7c9eaf
jsecchiero <secchierojacopo@gmail.com>
2019-07-15 19:09:39 +0200

    Build automatically the latest three kubespray versions

--- cicd/build.sh
+++ cicd/build.sh
@@ -24,8 +24,10 @@ if [ "$OVFTOOL_MD5" != "e521b64686d65de9e91288225b38d5da" ]; then
 fi
 
 # Create images
-
-KUBESPRAY_VERSION=$(grep kubespray_version defaults/main.yml | cut -d ' ' -f 2)
+source cicd/get_latest_kubespray_minor.sh
+KUBESPRAY_VERSION=$(get_latest_kubespray_minor $1)
+export KUBESPRAY_VERSION
+echo The kubespray version used will be ${KUBESPRAY_VERSION}
 KUBERNETES_VERSION=$(curl -sS https://raw.githubusercontent.com/kubernetes-sigs/kubespray/$KUBESPRAY_VERSION/inventory/sample/group_vars/k8s-cluster/k8s-cluster.yml | grep kube_version | cut -d ' ' -f 2)
 IMAGE_NAME=kubernetes-$KUBERNETES_VERSION-$TRAVIS_BUILD_NUMBER
 export IMAGE_NAME
---
+++ cicd/get_latest_kubespray_minor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+get_latest_kubespray_minor() {
+
+  LATEST_MINOR=$1
+
+  export TAGS=$(curl -s https://api.github.com/repos/kubernetes-sigs/kubespray/releases | jq -r .[].tag_name | sort -Vr) 
+
+  # Get latest of each minor
+  LATEST=""
+  NUMBER=1
+  for i in $TAGS; do
+    i=$(echo $i | sed 's/v//g')
+    MAJOR=$(echo $i | cut -d . -f 1)
+    MINOR=$(echo $i | cut -d . -f 2)
+    MICRO=$(echo $i | cut -d . -f 3)
+
+    if [ "${MAJOR}.${MINOR}" == "$LATEST" ]; then
+      continue
+    fi
+    LATEST="${MAJOR}.${MINOR}" 
+    if [ "$LATEST_MINOR" == "$NUMBER" ]; then
+      echo v${MAJOR}.${MINOR}.${MICRO}
+      break
+    fi
+
+    NUMBER=$(echo ${NUMBER} + 1 | bc )
+  done
+
+}
+
+export -f get_latest_kubespray_minor
