c9f605a8e9a67c19afa350f3ff4ea1b0c50ac3e7
Matsca09 <1948700+Matsca09@users.noreply.github.com>
2019-10-31 15:48:47 +0100

    add configuration for CI

--- cicd/test.sh
+++ cicd/test.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+# Export role path
+export TESTROLE_PATH=$(pwd)
+
 # Generate a SSH key for test instances
 ssh-keygen -q -N "" -f testkey.pem
 
 # Start molecule
-docker-compose run --rm -e TESTROLE_PATH=$(pwd) molecule molecule converge
+molecule converge
+
+if [ $? -ne 0 ]; then
+  echo "*** CLUSTER BUILD FAILED ***"
+  echo "*** Kubelet service log for master:"
+  docker exec testcluster-testcluster-0 journalctl -xe -u kubelet --no-pager
+  docker exec testcluster-testcluster-0 docker version
+  docker exec testcluster-testcluster-0 docker info
+  docker exec testcluster-testcluster-0 docker ps -a
+  echo ""
+  echo "*** Kubelet service log for worker:"
+  docker exec testcluster-testworker-0 journalctl -xe -u kubelet --no-pager
+  docker exec testcluster-testworker-0 docker version
+  docker exec testcluster-testworker-0 docker info
+  docker exec testcluster-testworker-0 docker ps -a
+  exit 1
+else
+  echo "*** CLUSTER BUILD COMPLETED ***"
+  sleep 20
+  docker exec testcluster-testcluster-0 kubectl --kubeconfig /etc/kubernetes/admin.conf get nodes -o wide
+  echo "*** Cleaning up..."
+  molecule destroy
+  echo "*** Done"
+  exit 0
+fi
+
+exit 2
