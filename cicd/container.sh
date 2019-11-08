b2e21aec6ec0a5f0898eff9473b71e1e6d70bd35
Attilio Greco <attilio.greco.gapmilano@gmail.com>
2019-11-08 12:50:04 +0100

    update container script

--- cicd/container.sh
+++ cicd/container.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 IMAGE_NAME=automium/service-kubernetes
+KUBERNETES_VERSION="1.15.3"
+TAG_NAME=kubernetes-v${KUBERNETES_VERSION}-${CI_COMMIT_SHORT_SHA}
 docker build --no-cache --pull --tag "$IMAGE_NAME" .
 docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:${CI_COMMIT_SHORT_SHA}"
 #docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
-docker push "${IMAGE_NAME}:kubernetes-${CI_COMMIT_SHORT_SHA}"
+docker push "${IMAGE_NAME}:${CI_COMMIT_SHORT_SHA}"
 #docker push "${IMAGE_NAME}:latest"
