15794085f4bce1c6466b4f6fef60604e995214dd
Attilio Greco <attilio.greco.gapmilano@gmail.com>
2019-11-08 12:15:13 +0100

    test image

--- cicd/container.sh
+++ cicd/container.sh
@@ -5,5 +5,5 @@ docker build --no-cache --pull --tag "$IMAGE_NAME" .
 docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:${CI_COMMIT_SHORT_SHA}"
 #docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
-docker push "${IMAGE_NAME}:${CI_COMMIT_SHORT_SHA}"
+docker push "${IMAGE_NAME}:kubernetes-${CI_COMMIT_SHORT_SHA}"
 #docker push "${IMAGE_NAME}:latest"
