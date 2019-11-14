5bac7f3b7c09e4da3a874412b64a031f84e82a16
Matsca09 <1948700+Matsca09@users.noreply.github.com>
2019-11-14 15:03:41 +0100

    add quick cluster verification in CI

--- cicd/test.sh
+++ cicd/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Functions
+cleanup(){
+  echo "*** Cleaning up..."
+  molecule destroy
+  echo "*** Done"
+}
+
 # Export role path
 export TESTROLE_PATH=$(pwd)
 
@@ -12,9 +19,9 @@ if [ $? -ne 0 ]; then
   echo "*** molecule test FAILED ***"
   exit 1
 fi
+
 # Start test cluster with molecule
 molecule converge
-
 if [ $? -ne 0 ]; then
   echo "*** CLUSTER BUILD FAILED ***"
   echo "*** Kubelet service log for master:"
@@ -29,16 +36,19 @@ if [ $? -ne 0 ]; then
   docker exec testcluster-testworker-0 docker info
   docker exec testcluster-testworker-0 docker ps -a
   sleep 10
-  molecule destroy
+  cleanup
   exit 1
-else
-  echo "*** CLUSTER BUILD COMPLETED ***"
-  sleep 20
-  docker exec testcluster-testcluster-0 kubectl --kubeconfig /etc/kubernetes/admin.conf get nodes -o wide
-  echo "*** Cleaning up..."
-  molecule destroy
-  echo "*** Done"
-  exit 0
 fi
 
-exit 2
+sleep 5
+
+# Launch cluster verification
+molecule verify
+if [ $? -ne 0 ]; then
+  echo "*** CLUSTER VERIFICATION FAILED ***"
+  cleanup
+  exit 1
+fi
+
+cleanup
+exit 0
