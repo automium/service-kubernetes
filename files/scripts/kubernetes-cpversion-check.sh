74e8b9033e3466ffa7ae439677edef321f1c49a7
Matsca09 <1948700+Matsca09@users.noreply.github.com>
2020-05-07 16:56:41 +0200

    fix cp version consul check script for consul

--- files/scripts/kubernetes-cpversion-check.sh
+++ files/scripts/kubernetes-cpversion-check.sh
@@ -4,10 +4,10 @@ set -e
 
 CONTENT="$(cat /etc/kubernetes/cpversion_check)"
 
-if [ ${CONTENT} == "True"]; then
+if [ "${CONTENT}" == "True" ]; then
   echo "Control plane version check passed "
   exit 0
-elif [ ${CONTENT} == "False"]; then
+elif [ "${CONTENT}" == "False"]; then
   echo "Control plane version check failed - node will execute control plane upgrade"
   exit 2
 else
